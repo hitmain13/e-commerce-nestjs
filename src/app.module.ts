4fb73cb86cbe9c80001c2685f47475805189a7dd
Fabio Matsumoto <fabio.matsumoto@br.experian.com>
2023-11-15 00:42:28 -0300

    feat: city module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AddressModule } from './address/address.module';
+import { CityModule } from './city/city.module';
 import { UserModule } from './user/user.module';
 
 @Module({
@@ -20,6 +21,7 @@ import { UserModule } from './user/user.module';
       migrationsRun: true,
     }),
     UserModule,
+    StateModule,
     AddressModule,
   ],
   controllers: [],
